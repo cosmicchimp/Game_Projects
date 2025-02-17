5c0901c50388dc11d177effd60ab17f74bb968ca
cosmicchimp <max.lambert429@gmail.com>
2025-02-16 18:51:48 -0700

    Creating a popup file to handle popups telling the users what's happening

--- BattleshipV2/src/popups.js
+++ BattleshipV2/src/popups.js
@@ -2,13 +2,14 @@ export function popups() {
   const popupModule = document.createElement("div");
   const popupText = document.createElement("h2");
   const closeButton = document.createElement("button");
-  closeButton.innerText = "Close";
+  closeButton.classList = "closeButton";
+  popupModule.classList.add("popupModule");
+  closeButton.innerText = "Continue";
   popupModule.appendChild(popupText);
   popupModule.appendChild(closeButton);
   function p1ShipPlace() {
     document.body.appendChild(popupModule);
     popupModule.style.position = "absolute";
-    popupModule.style.display = "block";
     popupText.innerText = "Player 1 place your ships";
     closeButton.addEventListener("click", () => {
       popupModule.style.display = "none";

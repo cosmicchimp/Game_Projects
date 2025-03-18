221aaa84bd47d11d6e83f26e07a86d3c5d723378
cosmicchimp <max.lambert429@gmail.com>
2025-03-17 23:18:31 -0700

    Small visual changes

--- memory_card/src/components/LandingScreen.jsx
+++ memory_card/src/components/LandingScreen.jsx
@@ -5,26 +5,26 @@ export function LandingScreen({ updateDisplayScreen }) {
   }
   return (
     <>
-      <div className="modalWrapper">
-        <div className="startGameModal">
-          <div>
-            <h1>Tutorial:</h1>
-          </div>
-          <div>
-            {" "}
-            <p>
-              This is a memory card game. The goal of the game is to click each
-              card, without choosing one that you have already previously
-              clicked. There are three levels of difficulty, each with more
-              cards than the last.
-            </p>
-          </div>
-          <div>
-            {" "}
-            <button onClick={handleClick}>Start</button>
-          </div>
+      {/* <div className="modalWrapper"> */}
+      <div className="startGameModal">
+        <div>
+          <h1>Tutorial:</h1>
+        </div>
+        <div>
+          {" "}
+          <p>
+            This is a memory card game. The goal of the game is to click each
+            card, without choosing one that you have already previously clicked.
+            There are three levels of difficulty, each with more cards than the
+            last.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <button onClick={handleClick}>Start</button>
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 }
